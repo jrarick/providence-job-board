8de56622064b7855a248089c24db4a4533bc74f5
Joshua Rarick <rarick.joshua.e@gmail.com>
2025-01-04 12:59:41 -0600

    initial formatting

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,15 @@
-import type { Config } from "drizzle-kit";
+import type { Config } from "drizzle-kit"
 
 export default {
-  out: "./drizzle",
-  schema: "./database/schema.ts",
-  dialect: "sqlite",
-  driver: "d1-http",
-  dbCredentials: {
-    databaseId: "your-database-id",
-    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-    token: process.env.CLOUDFLARE_TOKEN!,
-  },
-} satisfies Config;
+	out: "./drizzle",
+	schema: "./database/schema.ts",
+	dialect: "sqlite",
+	driver: "d1-http",
+	dbCredentials: {
+		databaseId: "your-database-id",
+		// biome-ignore lint/style/noNonNullAssertion: envs require non-null assertions
+		accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+		// biome-ignore lint/style/noNonNullAssertion: envs require non-null assertions
+		token: process.env.CLOUDFLARE_TOKEN!,
+	},
+} satisfies Config
