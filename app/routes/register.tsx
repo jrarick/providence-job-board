508f3bd316adb9063cf7a9e9b61a34ff0b530791
Joshua Rarick <rarick.joshua.e@gmail.com>
2025-01-04 20:35:14 -0600

    added shadcn config and initial register ui

---
+++ app/routes/register.tsx
@@ -0,0 +1,39 @@
+import { Form } from "react-router"
+import { Button } from "~/components/ui/button"
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "~/components/ui/card"
+import { Input } from "~/components/ui/input"
+import { Label } from "~/components/ui/label"
+
+export default function Register() {
+	return (
+		<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-24">
+			<div className="mx-auto max-w-sm">
+				<Card className="max-w-xl">
+					<CardHeader>
+						<CardTitle>Register</CardTitle>
+						<CardDescription>Create an account.</CardDescription>
+					</CardHeader>
+					<CardContent>
+						<Form className="flex flex-col gap-4">
+							<div>
+								<Label htmlFor="email">Email Address</Label>
+								<Input id="email" />
+							</div>
+							<div>
+								<Label htmlFor="password">Password</Label>
+								<Input type="password" id="password" />
+							</div>
+							<Button type="submit">Submit</Button>
+						</Form>
+					</CardContent>
+				</Card>
+			</div>
+		</div>
+	)
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,78 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 export default {
+	darkMode: ["class"],
 	content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: [
-					'"Inter"',
-					"ui-sans-serif",
-					"system-ui",
-					"sans-serif",
-					'"Apple Color Emoji"',
-					'"Segoe UI Emoji"',
-					'"Segoe UI Symbol"',
-					'"Noto Color Emoji"',
-				],
+				sans: ["ibm-plex-sans", "system-ui", "sans-serif"],
+				display: ["brandon-grotesque", "sans-serif"],
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+			colors: {
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				card: {
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))",
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))",
+				},
+				primary: {
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
+				},
+				body: {
+					DEFAULT: "hsl(var(--body))",
+					foreground: "hsl(var(--body-foreground))",
+				},
+				footer: {
+					DEFAULT: "hsl(var(--footer))",
+					foreground: "hsl(var(--footer-foreground))",
+				},
+				longform: {
+					DEFAULT: "hsl(var(--longform))",
+					foreground: "hsl(var(--longform-foreground))",
+				},
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				chart: {
+					"1": "hsl(var(--chart-1))",
+					"2": "hsl(var(--chart-2))",
+					"3": "hsl(var(--chart-3))",
+					"4": "hsl(var(--chart-4))",
+					"5": "hsl(var(--chart-5))",
+				},
 			},
 		},
 	},
-	plugins: [],
-} satisfies Config;
+	plugins: [
+		require("tailwindcss-animate"),
+		require("@tailwindcss/forms"),
+		require("@tailwindcss/typography"),
+	],
+} satisfies Config
